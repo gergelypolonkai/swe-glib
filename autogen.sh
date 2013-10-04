7463b0e5cde3d984d61bcc340f627b60c06face6
Gergely Polonkai (W00d5t0ck) <gergely@polonkai.eu>
2013-10-05 00:57:38 +0200

    Patch autogen.sh to make GTK-Doc really optional
    • gnome-autogen.sh does’t recognize the need for GTK-Doc if GTK_DOC_CHECK
      is not present at the beginning of a line of configure.ac (which is
      the case here)
    
    • autogen.sh now takes care of running gtkdocize, thus copying over
      gtk-doc.make. If GTK-Doc is not present, a fake gtk-doc.make is
      generated, so automake doesn’t complain anymore

--- autogen.sh
+++ autogen.sh
@@ -13,6 +13,18 @@ PKG_NAME="swe-glib"
     exit 1
 }
 
+GTKDOCIZE=$(which gtkdocize 2>/dev/null)
+if test -z $GTKDOCIZE; then
+        echo "You don't have gtk-doc installed, and thus won't be able to generate the documentation."
+        rm -f gtk-doc.make
+        cat > gtk-doc.make <<EOF
+EXTRA_DIST =
+CLEANFILES =
+EOF
+else
+        gtkdocize || exit $?
+fi
+
 which gnome-autogen.sh || {
     echo "gnome-autogen.sh not found, you need to install gnome-common"
     exit 1
